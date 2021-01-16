fd168222aa45a9b95d7c225a104001639e3efebc
Robert Williams <rcw0086@gmail.com>
2021-01-16 10:38:14 -0600

    ch. 6 complete, minus exercises

--- catalog/views.py
+++ catalog/views.py
@@ -35,6 +35,11 @@ def index(request):
 from django.views import generic
 class BookListView(generic.ListView):
     model = Book
+
+    # pagination is built in 
+    # creates `is_paginated` method and and `page_obj` methods for use in templates
+    paginate_by = 10
+
     # context_object_name = 'my_book_list'   # your own name for the list as a template variable
     # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
     # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
